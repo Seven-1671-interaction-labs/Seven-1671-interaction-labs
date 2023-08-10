3871874658bde2351715f33fd18f609c39208a22
Seven-1671-interaction-labs <chakkapat.w@kkumail.com>
2023-08-10 10:56:29 +0700

    add

--- 1671-lab1-prob4.py
+++
@@ -1,3 +0,0 @@
-import math
-for x in range(1, 11):
-    print(f'{x} {x*2:} {2*math.pi*x*(x+(x*2)):.2f}')
--- Demo Problem 4.py
+++
@@ -1,3 +0,0 @@
-def get__net__pirce(price, tax=0.07, discount=0.05):
-final_price = price * (1 + tax - discount)
-print(f"For the intinal")
